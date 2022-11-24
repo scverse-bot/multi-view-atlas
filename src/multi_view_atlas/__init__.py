5b4579b948821b547b92f6b7f50fff80309cf43a
Emma Dann <ed6@sanger.ac.uk>
2022-11-24 14:56:05 +0000

    better logger

--- src/multi_view_atlas/__init__.py
+++ src/multi_view_atlas/__init__.py
@@ -1,6 +1,17 @@
+import logging
 from importlib.metadata import version
 
+from rich.logging import RichHandler
+
 from . import pl, pp, tl, utils
-from .config import set_options
+
+logger = logging.getLogger(__name__)
+logger.propagate = False
+ch = RichHandler(level=logging.INFO, show_path=False, show_time=False)
+formatter = logging.Formatter("%(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.removeHandler(logger.handlers[0])
+
 
 __version__ = version("multi-view-atlas")
