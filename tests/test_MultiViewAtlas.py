9364f8587ef0592d760a2124d2e40ac7ee5efc22
Emma Dann <ed6@sanger.ac.uk>
2022-12-13 10:26:41 +0000

    added test for view_assign table from mudata

--- tests/test_MultiViewAtlas.py
+++ tests/test_MultiViewAtlas.py
@@ -111,6 +111,24 @@ def test_transition_rule():
     assert mva.view_transition_rule.loc["B cells", child_v] is not None, "Extra transition rule"
 
 
+def test_view_assign_building():
+    """Test that when initializing from MuData the view_assignment table is correct"""
+    adata = sample_dataset()
+    view_assign = adata.obsm["view_assign"].copy()
+    adata_dict = {}
+    del adata.obsm["view_assign"]
+    adata_dict["full"] = adata.copy()
+    for v in view_assign:
+        adata_dict[v] = adata[view_assign[v] == 1].copy()
+    for v in adata_dict.keys():
+        assert "view_assign" not in adata_dict[v].obsm
+    mdata = mudata.MuData(adata_dict)
+    mva = MultiViewAtlas(mdata, transition_rule="louvain", rename_obsm=True)
+    for v in mva.views:
+        cells = mva.mdata.obs_names[mva.mdata.obsm["view_assign"][v] == 1]
+        assert cells.isin(adata_dict[v].obs_names).all()
+
+
 def test_update_views():
     adata = sample_dataset()
     mva = MultiViewAtlas(adata)
